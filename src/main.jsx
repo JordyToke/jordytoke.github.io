e7890413e3fe24633d76037567dc81c93853e47a
JordyToke <jordyn.toke@gmail.com>
2024-12-09 21:49:22 +1100

    Added react router, and helmet with some head meta tags

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,23 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from 'react';
+import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
+import './index.css';
+import App from './App.jsx';
 
-createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <BrowserRouter>
+      <HelmetProvider>
+        <App>
+          <Helmet prioritizeSeoTags>
+            <title>Jorbys World</title>
+            <meta property="og:title" content="Jorbys World" />
+            <meta property="og:description" content="Jordy Toke's Portfolio Website" />
+            <meta property="og:locale" content="en_AU" />
+          </Helmet>
+        </App>
+      </HelmetProvider>
+    </BrowserRouter>
   </StrictMode>,
 )

14577233b2c1983a3c7871cd81c59050c7a850b0
JordyToke <jordyn.toke@gmail.com>
2025-01-31 12:58:25 +1100

    added home as its own page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,9 @@ import React from "react";
 
 export default function Home() {
   return (
-    <p>Hello World</p>
+    <>
+      <h2>This is the home page!</h2>
+      <p>Hello World! Welcome to my homepage</p>
+    </>
   )
 }

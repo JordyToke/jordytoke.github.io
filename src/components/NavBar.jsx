d2433ebf80e0e6976a84b1b790d6759e0b3fc62d
JordyToke <jordyn.toke@gmail.com>
2025-01-31 12:57:13 +1100

    created navbar component

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,23 @@
+import React from "react";
+import style from "./NavBar.module.css";
+
+
+
+const NavList = () => {
+
+}
+
+export default function NavBar() {
+
+
+
+  return (
+    <nav>
+      <ul>
+        <li><a href="/">Home</a></li>
+        <li><a href="/About">About</a></li>
+        <li><a href="/Contact">Contact</a></li>
+      </ul>
+    </nav>
+  )
+}
